983c16d38d5ca1c6974181196b0dbe2b143e1b70
ajomuch92 <aaronmontes_92@yahoo.es>
2022-04-17 13:06:57 -0600

    feat: 🎸 Creating initial project with Deno js

---
+++ deps.ts
@@ -0,0 +1,3 @@
+import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+
+export { Router, Application };
---
+++ index.ts
@@ -0,0 +1,18 @@
+import { Application } from './deps.ts';
+import router from './routes.ts';
+
+
+const env = Deno.env.toObject()
+const PORT = env.PORT || 3000;
+const HOST = env.HOST || 'localhost';
+
+const app = new Application();
+
+// app.use(errorHandler);
+app.use(router.routes());
+app.use(router.allowedMethods());
+// app.use(_404);
+
+console.log(`Server running on port ${PORT}`);
+
+await app.listen(`${HOST}:${PORT}`);
