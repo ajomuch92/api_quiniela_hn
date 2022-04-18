4a58a3946ae7e85ad4065b31fe969273aba6c563
ajomuch92 <aaronmontes_92@yahoo.es>
2022-04-17 19:16:48 -0600

    feat: 🎸 Adding more dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
-
-export { Router, Application };
+export { Application,Router } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
+export type { RouterContext } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
+export { Database, MySQLConnector, DataTypes, Model } from 'https://deno.land/x/denodb@v1.0.40/mod.ts';
+export type { FieldProps } from 'https://deno.land/x/denodb@v1.0.40/lib/data-types.ts';
