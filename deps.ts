89681538b5fba9469207b6aefc70e056720a2077
ajomuch92 <aaronmontes_92@yahoo.es>
2022-04-23 20:46:03 -0600

    feat: 🎸 Adding env dependency and files

--- deps.ts
+++ deps.ts
@@ -2,3 +2,4 @@ export { Application,Router } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
 export type { RouterContext } from 'https://deno.land/x/oak@v10.5.1/mod.ts';
 export { Database, MySQLConnector, DataTypes, Model } from 'https://deno.land/x/denodb@v1.0.40/mod.ts';
 export type { FieldProps } from 'https://deno.land/x/denodb@v1.0.40/lib/data-types.ts';
+export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
